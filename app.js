a2cc20cd0982aa902597eb0c930ea271c98d2b4a
Andrey <teryaev.2004@mail.ru>
2023-01-13 12:46:30 +0300

    change-and-finish

--- app.js
+++ app.js
@@ -6,34 +6,41 @@ const rusLetters = "абвгдеёжзийклмнопрстуфхцчшщъыь
 const numbers = "123456789".split("");
 const allValues = engLetters.concat(rusLetters, numbers);
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 const bg = "#000";
+const fontSize = 20;
 
-const randomValues = (arr = []) => {
-  let value = Math.floor(Math.random() * arr.length);
+const columns = canvas.width / 20;
+let arr = [];
 
-  return arr[value];
-}
+for(let i = 0; i < columns; i++) arr[i] = 1;
 
-randomValues();
+console.log(arr);
+
+const randomValue = (arr) => {
+  const random = Math.floor(Math.random() * allValues.length);
+  
+  return arr[random];
+};
 
 const draw = () => {
-  ctx.beginPath();
-  ctx.fillStyle = bg;
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const group = [];
-  const groupLength = Math.floor(Math.random() * (10 - 1) + 1);
+  ctx.fillStyle = "#0f0";
+  ctx.font = `${fontSize}px serif`;
 
-  while(group.length < groupLength) {
-    group.push(randomValues(allValues));
+  for(let i = 0; i < arr.length; i++) {
+    let str = allValues[Math.floor(Math.random() * allValues.length)];
+
+    ctx.fillText(str, i * fontSize, arr[i] * fontSize);
+
+    if(arr[i] * fontSize > canvas.height && Math.random() > 0.9) arr[i] = 0;
+
+    arr[i]++
   }
-
-  requestAnimationFrame(draw);
 }
 
-const resizeCanvas = () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-}
-
-draw();
+setInterval(draw, 50);
